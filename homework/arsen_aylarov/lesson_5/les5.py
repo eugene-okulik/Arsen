c30b609e30c025f76a3182fb4ce00bd13d35eb62
Arsen <arsen15vlz@gmail.com>
2026-02-05 11:56:51 +0300

    'upd_HW'

--- homework/arsen_aylarov/lesson_5/les5.py
+++ homework/arsen_aylarov/lesson_5/les5.py
@@ -4,17 +4,12 @@ name, last_name, city, phone, country = person
 res1 = 'результат операции: 42'
 res2 = 'результат операции: 514'
 res3 = 'результат работы программы: 9'
-a = res3.split()
-b = res2.split()
-c = res1.split()
-d = int((a[-1]))
-e = int((b[-1]))
-f = int((c[-1]))
-print(d + 10, e + 10, f + 10)
+a = int(res3.split()[-1])
+b = int(res2.split()[-1])
+c = int(res1.split()[-1])
+print(a + 10, b + 10, c + 10)
 
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
 subjects = ['math', 'biology', 'geography']
-st1, st2, st3 = students
-sub1, sub2, sub3 = subjects
-print(f'Students {st1}, {st2}, {st3} study these subjects: {sub1}, {sub2}, {sub3}')
+print('Students', ' '.join(students), 'study these subjects:', ' '.join(subjects) )
