21d97e801b7c4ff2154f1d493f69e81283f0e1d6
Arsen <arsen15vlz@gmail.com>
2026-02-07 15:30:49 +0300

    'hw_les7'

---
+++ homework/arsen_aylarov/while_and_func/aylarov_HW7_1.py
@@ -0,0 +1,12 @@
+import random
+
+
+i = random.randrange(1, 4)
+
+while i :
+    numb = int(input('enter a number :'))
+    if i == numb:
+        break
+    elif i != numb:
+        print('попробуйте снова!')
+print('поздравляю, вы угадали!!!')
