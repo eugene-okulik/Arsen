1c2ceaa7170214ae962df51b955d47905e290818
Arsen <arsen15vlz@gmail.com>
2026-02-17 17:13:36 +0300

    'upd_HW'

--- homework/arsen_aylarov/les9/maping_and_data.py
+++ homework/arsen_aylarov/les9/maping_and_data.py
@@ -1,17 +1,7 @@
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27,
                 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 hot_days = (list(filter(lambda temp: temp > 28, temperatures)))
-hot_day = []
-cold_day = []
-days = []
-for temp in hot_days:
-    if temp >= 32:
-        hot_day.append(temp)
-    elif temp <= 31 and temp >= 30:
-        days.append(temp)
-    else:
-        cold_day.append(temp)
-
-print(hot_day)
-print(cold_day)
-print(days)
+print(hot_days)
+print(max(hot_days))
+print(min(hot_days))
+print(round(sum(hot_days)/len(hot_days), 1))
