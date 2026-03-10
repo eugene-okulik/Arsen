28188e1d20c0b5d3bd6cb26ef97a6e6766c58aff
Arsen <arsen15vlz@gmail.com>
2026-03-10 19:16:15 +0300

    'hw_upd'

--- homework/arsen_aylarov/less12/less12.py
+++ homework/arsen_aylarov/less12/less12.py
@@ -1,72 +1,78 @@
 class Flower:
-    def __init__(self, flower, petals, price, time_of_life, stem_len, color):
+    def __init__(self, flower, petals, price, time_of_life, stem_len, color, variety):
         self.flower = flower
         self.petals = petals
         self.time_of_life = time_of_life
         self.price = price
         self.stem_len = stem_len
         self.color = color
+        self.variety = variety
 
+    def __repr__(self):
+        return f'{self.variety}' #{self.color}, {self.price}, {self.stem_len}]'
+
+    def __str__(self):
+        return self.__repr__()
 
 class Roses(Flower):
     def __init__(self, flower, petals, price, time_of_life, stem_len, color, variety, thorns):
-        super().__init__(flower, petals, price, time_of_life, stem_len, color)
+        super().__init__(flower, petals, price, time_of_life, stem_len, color, variety)
         self.thorns = thorns
-        self.variety = variety
+
 
 
 class Pions(Flower):
-    def __init__(self, flower, petals, price, time_of_life, stem_len, color, gibrid, variety):
-        super().__init__(flower, petals, price, time_of_life, stem_len, color)
+    def __init__(self, flower, petals, price, time_of_life, stem_len, color, variety, gibrid ):
+        super().__init__(flower, petals, price, time_of_life, stem_len, color, variety)
         self.gibrid = gibrid
-        self.variety = variety
+
+
+
+
+
+
+
 
 
 rose = Roses(True, True, 120, 10, 80, 'Red',
              'Koko Loko', True,)
 rose_yellow = Roses(True, True, 150, 5, 80, 'yellow',
                     'yellow', True,)
-pion = Pions(True, True, 180, 12, 60, 'pinc', True,
-             'sarah_bernhardt')
+pion = Pions(True, True, 180, 12, 60, 'pinc', 'sarah_bernhardt',
+             True)
 buket = [rose, rose_yellow, pion]
-select_mod = (input('выбирите способ фильтрации '))
 
 
 class Buqet:
     def __init__(self, buket):
         self.buket = buket
 
-    def buqet(self):
+    def average_life(self):
         total = 0
         for x in self.buket:
             total += x.time_of_life
         average = total / len(self.buket)
-        return print(f'среднее время жизни букета : {int(average)} дней')
+        return average
 
     def price(self):
-        price = sorted(self.buket, key=lambda x: x.price)
-        return [f"{x.variety}: {x.price} руб." for x in price]
+        return sorted(self.buket, key=lambda x: x.price)
+
 
     def stems_len(self):
-        stens = sorted(self.buket, key=lambda x: x.stem_len)
-        return [f"{x.variety}: {x.stem_len} см." for x in stens]
+        return sorted(self.buket, key=lambda x: x.stem_len)
+
 
     def flower_color(self):
-        f_color = sorted(self.buket, key=lambda x: x.color)
-        return [f"{x.variety}: {x.color} цвет" for x in f_color]
+        return  sorted(self.buket, key=lambda x: x.color)
 
-    def choose(self, choice):
-        if choice == 'время':
-            return self.buket()
-        if choice == 'цена':
-            return self.price()
-        if choice == 'длинна':
-            return self.stems_len()
-        if choice == 'цвет':
-            return self.flower_color()
-        else:
-            return None
+    def find_flower(self, s_len=None, colors=None, min_price=None ):
+        result = []
+        for flower in self.buket:
+            if (colors is None or flower.color == colors) and (min_price is None or flower.price <= min_price) \
+                and (s_len is None or flower.stem_len <= s_len):
+                result.append(flower)
+        return result
 
+my_buqet = Buqet(buket)
 
-my_bouquet = Buqet(buket)
-print(f'{my_bouquet.choose(select_mod)}')
+print(my_buqet.find_flower(colors='Red'))
